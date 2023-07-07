bb8679e51e5f27ec24ff2dba9b28c05f2973f2ac
saminenipraveenyalakshmi <saminenipraveenyalakshmi@gmail.com>
2023-07-07 15:55:38 -0400

    added second heading

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>This line is added by praveenya in GitHub</h1>
+      <h1>This second line is added by praveenya from local repository</h1>
     </div>
   );
 }
